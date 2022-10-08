6e0062bd7f9d354d3e731f28107e5aa0677a997e
eugeneiee <112212472+eugeneiee@users.noreply.github.com>
2022-10-08 16:13:38 +0100

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,91 @@
+test = input("Start the test?\n")
+if test == ("yes"):
+    print("Lets begin")
+if test == ("no"):
+        print("Shutting down")
+        (end)
+
+print("1st question") #question 1
+q1 = input("What does a sytnax mean?\n")
+if q1 == ("Words in a specific order"):
+    print("Correct")
+else:
+    again = input("Incorrect, would you like to try again?\n")
+    if again == ("yes"):
+     print("Okay lets try this again")
+    if again == ("no"):
+        print("Shutting down!")
+        (end)
+
+
+print("2nd question") #question 2
+q2 = input("What does a variable mean?\n")
+if q2 == ("A way of storing data"):
+    print("Correct")
+else:
+    again = input("Incorrect, would you like to try again?\n")
+    if again == ("yes"):
+     print("Okay lets try this again")
+    if again == ("no"):
+        print("Shutting down!")
+        (end)
+
+
+print("3rd question") #question 3
+q3 = input("What does a string mean?\n")
+if q3 == ("A list of numbers or characters"):
+    print("Correct")
+else:
+    again = input("Incorrect, would you like to try again?\n")
+    if again == ("yes"):
+     print("Okay lets try this again")
+    if again == ("no"):
+        print("Shutting down!")
+        (end)
+
+
+print("4th question") #question 4
+q4 = input("What does a interger mean?\n")
+if q4 == ("A whole number"):
+    print("Correct")
+else:
+    again = input("Incorrect, would you like to try again?\n")
+    if again == ("yes"):
+     print("Okay lets try this again")
+    if again == ("no"):
+        print("Shutting down!")
+        (end)
+
+
+print("5th question") #question 5
+q5 = input("What does a float mean?\n")
+if q5 == ("A number with a decimal point"):
+    print("Correct")
+else:
+    again = input("Incorrect, would you like to try again?\n")
+    if again == ("yes"):
+     print("Okay lets try this again")
+    if again == ("no"):
+        print("Shutting down!")
+        (end)
+
+
+print("6th question") #question 6
+q6 = input("What does a booelan mean?\n")
+if q6 == ("A data type with two values"):
+    print("Correct")
+else:
+    again = input("Incorrect, would you like to try again?\n")
+    if again == ("yes"):
+     print("Okay lets try this again")
+    if again == ("no"):
+        print("Shutting down!")
+        (end)
+
+#id lie to add a second pause inbetween the questions
+#implement a username and password to get in
+#make it timed
+#how many did it get correct
+# end is not defined  - why?
+
+
